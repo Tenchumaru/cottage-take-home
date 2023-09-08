c5ff827efcfc9214664479ce997c401888a43d93
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2023-09-08 14:19:55 -0700

    Check for circular dependencies

--- process-phases.ts
+++ process-phases.ts
@@ -38,25 +38,47 @@ async function main(filePaths: string[]): Promise<void> {
     throw new Error('missing phases');
   }
 
-  // Sort the phases by prerequisites.
+  // Sort the phases by prerequisites and check for circular dependencies.
   phases.sort(compare);
+  validate(phases);
 
-  // Sort the stages of each phase by prerequisites and print them.
+  // Sort the stages of each phase by prerequisites and check for circular dependencies.
+  phases.forEach((phase) => {
+    const stages = dict[phase.name.toLowerCase()];
+    stages.sort(compare);
+    validate(stages);
+  });
+
+  // Print the phases and stages.
   phases.forEach((phase, phaseIndex) => {
     console.log(phaseIndex + 1, phase.name);
     const stages = dict[phase.name.toLowerCase()];
-    stages.sort(compare);
     stages.forEach((stage, stageIndex) => {
       console.log(`${phaseIndex + 1}.${stageIndex + 1}`, stage.name);
     });
   });
 
+  // Compare two stages by checking their prerequisites.
   function compare(a: Stage, b: Stage): number {
     if (a.prerequisites.includes(b.name)) {
+      // b is a prerequisite of a so a comes after it.
       return 1;
     } else if (b.prerequisites.includes(a.name)) {
+      // a is a prerequisite of b so b comes after it.
       return -1;
     }
+
+    // Neither is a prerequisite of the other so they are essentially equivalent.
     return 0;
+  }
+
+  // Validate the array of stages by checking if there is a violation of the prerequisites.  If there is, that means there is a
+  // circular dependency.
+  function validate(stages: Stage[]) {
+    stages.forEach((stage, index) => {
+      if (stage.prerequisites.some((prerequisite) => stages.slice(index).some((laterStage) => laterStage.name === prerequisite))) {
+        throw new Error('circular dependency');
+      }
+    })
   }
 }
